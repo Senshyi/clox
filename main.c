c88f08030e105a378c0c56e4c5cbc33475dc0b77
ayhonz <janmachacek97@gmail.com>
2024-04-18 15:35:17 +0200

    make it calculate!

--- main.c
+++ main.c
@@ -9,13 +9,25 @@ int main(int argc, char* argv[])
     Chunk chunk;
     initChunk(&chunk);
 
-    int constant = addConstant(&chunk, 1.2);
+    int constant = addConstant(&chunk, 1);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
-    writeChunk(&chunk, OP_NEGATE, 123);
+
+    constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_MULTIPLY, 123);
+
+    constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_ADD, 123);
 
     writeChunk(&chunk, OP_RETURN, 123);
     disassembleChunk(&chunk, "test chunk");
+
     interpret(&chunk);
     freeVM();
     freeChunk(&chunk);
